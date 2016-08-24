7792e4ef2d570cf94bab83e04c55f65c0bbf7580
Jean-Paul PETILLON <Art-ut-Kia@users.noreply.github.com>
2016-08-24 19:14:33 +0200

    changed some tabs into spaces

--- Source-Files/pulse32.vhd
+++ Source-Files/pulse32.vhd
@@ -8,10 +8,12 @@ use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
 
 entity pulse32 is
-    Port ( clk       : in  std_logic;
-           count     : in  std_logic_vector(31 downto 0);
-           threshold : in  std_logic_vector(31 downto 0);
-           pulse     : out std_logic);
+   Port(
+      clk       : in  std_logic;
+      count     : in  std_logic_vector(31 downto 0);
+      threshold : in  std_logic_vector(31 downto 0);
+      pulse     : out std_logic
+   );
 end pulse32;
 
 architecture behavioral of pulse32 is
@@ -20,9 +22,9 @@ signal ipulse: std_logic := '0';
 begin
    lower <= '1' when (count<threshold) else '0';
    process (clk) begin
-	   if (clk = '1' and clk'event) then
-		   ipulse <= lower; 
-		end if;
+      if (clk = '1' and clk'event) then
+         ipulse <= lower; 
+      end if;
    end process;
    pulse <= ipulse;   
 end behavioral;
