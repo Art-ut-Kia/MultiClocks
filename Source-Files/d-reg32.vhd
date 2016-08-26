dc313ad2a38a420aafe4d003cce3d3922c51c81a
Jean-Paul PETILLON <Art-ut-Kia@users.noreply.github.com>
2016-08-26 15:21:44 +0200

    Update d-reg32.vhd

--- Source-Files/d-reg32.vhd
+++ Source-Files/d-reg32.vhd
@@ -1,6 +1,7 @@
 ----------------------------------------------------------------------------------
 -- d_reg32.vhd: 32-bit D flip-flop
 --              memorizes a 32-bit word on the rising edge of a clock
+-- author: JPP
 ----------------------------------------------------------------------------------
 library ieee;
 use ieee.numeric_std.all, ieee.std_logic_1164.all, ieee.std_logic_unsigned.all;
@@ -18,13 +19,11 @@ entity d_reg32 is
 end d_reg32;
 
 architecture behavioral of d_reg32 is
-signal iq: std_logic_vector(31 downto 0) := std_logic_vector(to_unsigned(q0,32));
+   signal iq: std_logic_vector(31 downto 0) := std_logic_vector(to_unsigned(q0,32));
 begin
    process(clk) begin
-      if clk = '1' and clk'event then
-         if en = '1' then
-            iq <= d;
-         end if;
+      if clk = '1' and clk'event and en = '1' then
+         iq <= d;
       end if;
    end process;
    q <= iq;
