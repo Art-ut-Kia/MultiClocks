740bdcf8e4a3082a4a8bd51222ccf26dbc8a63fb
Jean-Paul PETILLON <Art-ut-Kia@users.noreply.github.com>
2016-08-28 17:35:10 +0200

    Update SpiRx40_TB.vhd

--- Source-Files/SpiRx40_TB.vhd
+++ Source-Files/SpiRx40_TB.vhd
@@ -1,35 +1,32 @@
 --------------------------------------------------------------------------------
--- VHDL Test Bench Created by ISE for module: SpiRx40
+-- VHDL Test Bench Created by ISE for SpiRx40 module
 --------------------------------------------------------------------------------
-LIBRARY ieee;
-USE ieee.std_logic_1164.ALL;
---USE ieee.numeric_std.ALL;
- 
-ENTITY SpiRx40_TB IS
-END SpiRx40_TB;
- 
-ARCHITECTURE behavior OF SpiRx40_TB IS 
-    -- Component Declaration for the Unit Under Test (UUT)
-    COMPONENT SpiRx40
-    PORT(
-       clk : IN  std_logic;
-       mosi : IN  std_logic;
-       ctrlout : OUT  std_logic_vector(7 downto 0);
-       dataout : OUT  std_logic_vector(31 downto 0)
-    );
-    END COMPONENT;
+library ieee;
+use ieee.std_logic_1164.ALL;
 
+entity SpiRx40_TB IS
+end SpiRx40_TB;
+ 
+architecture behavior OF SpiRx40_TB IS 
+   -- Component Declaration for the Unit Under Test (UUT)
+   component SpiRx40
+   port(
+      clk : in  std_logic;
+      mosi : in  std_logic;
+      ctrlout : out  std_logic_vector(7 downto 0);
+      dataout : out  std_logic_vector(31 downto 0)
+   );
+   end component;
    --Inputs
    signal clk : std_logic := '0';
    signal mosi : std_logic := 'Z';
-
- 	--Outputs
+   --Outputs
    signal ctrlout : std_logic_vector(7 downto 0);
    signal dataout : std_logic_vector(31 downto 0);
 
-BEGIN
+begin
    -- Instantiate the Unit Under Test (UUT)
-   uut: SpiRx40 PORT MAP (
+   uut: SpiRx40 port MAP (
       clk => clk,
       mosi => mosi,
       ctrlout => ctrlout,
@@ -38,26 +35,26 @@ BEGIN
 
    -- Stimulus process
    stim_proc: process
-	type Msg_T is array (0 to 4) of std_logic_vector(7 downto 0);
-	variable msg : Msg_T :=
-      (x"01",
-	    x"02",
-		 x"03",
-		 x"04",
-		 x"05");
+      type Msg_T is array (0 to 4) of std_logic_vector(7 downto 0);
+      variable msg : Msg_T := (
+         x"01",
+         x"02",
+         x"03",
+         x"04",
+         x"05"
+      );
    begin		
       wait for 50 ns;
       for bytecount in 0 to 4 loop
          for bitcount in 7 downto 0 loop
-			  mosi <= msg(bytecount)(bitcount);
-			  wait for 50 ns;
-			  clk <= '1';
-			  wait for 50 ns;
-			  clk <= '0';
+            mosi <= msg(bytecount)(bitcount);
+            wait for 50 ns;
+            clk <= '1';
+            wait for 50 ns;
+            clk <= '0';
          end loop;
       end loop;
-		mosi <= 'Z';
+         mosi <= 'Z';
       wait;
    end process;
-
-END;
+end;
