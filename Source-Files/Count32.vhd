13b1cc262c9dc19264621f967d114062711a3256
Jean-Paul PETILLON <Art-ut-Kia@users.noreply.github.com>
2019-12-03 16:02:08 +0100

    updated after tuning
    added an "enable" input

--- Source-Files/Count32.vhd
+++ Source-Files/Count32.vhd
@@ -1,6 +1,7 @@
 ----------------------------------------------------------------------------------
--- count32.vhd: 32-bit counter
---              counts from 0 to maxcount-1
+-- count32.vhd: 32-bit counter with enable
+--              counts from 0 to maxcount-1 and then rollover
+-- author : JPP
 ----------------------------------------------------------------------------------
 library ieee;
 use ieee.std_logic_1164.all;
@@ -9,6 +10,7 @@ use ieee.std_logic_unsigned.all;
 entity count32 is
    port(
       clk      : in  std_logic;
+		enable   : in  std_logic;
       maxcount : in  std_logic_vector(31 downto 0);
       count    : out std_logic_vector(31 downto 0)
    );
@@ -18,11 +20,13 @@ architecture behavioral of count32 is
    signal icnt: std_logic_vector(31 downto 0) := (others => '0');
 begin
    process(clk) begin
-      if (clk = '1' and clk'event) then
-         if (icnt >= maxcount) then
-            icnt <= (others => '0');
-         else
-            icnt <= icnt + "1";
+      if rising_edge(clk) then
+         if enable = '1' then 
+            if (icnt >= maxcount) then
+               icnt <= (others => '0');
+            else
+               icnt <= icnt + "1";
+            end if;
          end if;
       end if;
    end process;
