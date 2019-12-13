88cb3ac49c9eed7f41e22b412150e26f3b6e3f5a
Jean-Paul PETILLON <Art-ut-Kia@users.noreply.github.com>
2019-12-13 12:54:13 +0100

    upgrade after tuning
    completed test bench with "enable" signal stimulus

--- Source-Files/count32_TB.vhd
+++ Source-Files/count32_TB.vhd
@@ -8,29 +8,40 @@ entity count32_TB is
 end count32_TB;
  
 architecture behavior of count32_TB is 
+
    -- Component Declaration for the Unit Under Test (UUT)
+ 
    component count32
       port(
          clk : IN  std_logic;
+         enable : IN  std_logic;
          maxcount : IN  std_logic_vector(31 downto 0);
          count : OUT  std_logic_vector(31 downto 0)
       );
    end component;
+    
+
    --Inputs
    signal clk : std_logic := '0';
+   signal enable : std_logic := '0';
    signal maxcount : std_logic_vector(31 downto 0) := "00000000000000000000000000001111";
+
    --Outputs
    signal count : std_logic_vector(31 downto 0);
+
    -- Clock period definitions
    constant clk_period : time := 10 ns;  -- 100MHz
  
 begin
-   -- Instantiate the Unit Under Test (UUT)
+ 
+	-- Instantiate the Unit Under Test (UUT)
    uut: count32 port map (
       clk => clk,
+		enable => enable,
       maxcount => maxcount,
       count => count
    );
+
    -- Clock process definitions
    clk_process :process
    begin
@@ -39,13 +50,13 @@ begin
       clk <= '1';
       wait for clk_period/2;
    end process;
-   -- Stimulus process
+
+   -- Enable signal generation process
    stim_proc: process
    begin		
-      -- hold reset state for 100 ns.
-      wait for 100 ns;
-      wait for clk_period*10;
-      -- insert stimulus here
+      wait for clk_period*10.25;
+      enable <= '1';		
       wait;
    end process;
+
 end;
