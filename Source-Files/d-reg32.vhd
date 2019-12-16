883af00e052724072aeb2fbcf7ef9a3fc51d312d
Jean-Paul PETILLON <Art-ut-Kia@users.noreply.github.com>
2019-12-16 19:26:42 +0100

    minor changes
    coding rules

--- Source-Files/d-reg32.vhd
+++ Source-Files/d-reg32.vhd
@@ -1,15 +1,17 @@
 ----------------------------------------------------------------------------------
 -- d_reg32.vhd: 32-bit D flip-flop
 --              memorizes a 32-bit word on the rising edge of a clock
--- author: JPP
+-- author : JPP
 ----------------------------------------------------------------------------------
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all; 
 
 entity d_reg32 is
    generic(
-      q0  : integer := 1E6 -- init value (default = 1M to generate 100Hz from 100MHz)
-   );
+	   -- default period : 100MHz/1M = 100Hz
+	   q0 : integer := 1e6
+	);
    port(
       clk : in  std_logic;
       en  : in  std_logic;
@@ -19,10 +21,12 @@ entity d_reg32 is
 end d_reg32;
 
 architecture behavioral of d_reg32 is
+   signal iq: std_logic_vector(31 downto 0) := std_logic_vector(to_unsigned(q0, 32));
 begin
    process(clk, en) begin
-      if clk = '1' and clk'event and en = '1' then
-         q <= d;
+	   if rising_edge(clk) and en = '1' then
+         iq <= d;
       end if;
    end process;
+	q <= iq;
 end behavioral;
